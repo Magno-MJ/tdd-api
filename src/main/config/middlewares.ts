8ba8ff46105580eea83a523bbd8c4700f5a052fc
Magno <magno.jovino@hotmail.com>
2022-07-12 20:44:58 -0300

    feat: ensure CORS is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,7 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/body-parser'
+import { cors } from '../middlewares/cors'
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
