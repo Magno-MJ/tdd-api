5ea07a8e900d06a5084b38f450c50914c5c287de
Magno <magno.jovino@hotmail.com>
2022-07-12 21:48:35 -0300

    feat: ensure signup up returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
 import { setupMiddlewares } from './middlewares'
+import { setupRoutes } from './routes'
 export const app = express()
 setupMiddlewares(app)
+setupRoutes(app)
