8c15f56ac36bd38b4d543f1d95d60136ac142d5c
Magno <magno.jovino@hotmail.com>
2022-07-13 19:56:08 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -4,8 +4,8 @@ import { AddAccountModel } from '../../../../domain/use-cases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
-    return MongoHelper.map(result.ops[0])
+    return MongoHelper.map((await result).ops[0])
   }
 }
